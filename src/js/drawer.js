3f3f85a19eb279059ac2dd54622023b1dacfa845
webonds2081 <webonds2081@gmail.com>
2021-07-16 00:17:33 +0900

    first commit

---
+++ src/js/drawer.js
@@ -0,0 +1,11 @@
+
+$(function () {
+  $("#MenuButton").click(function () {
+    // $(".l-drawer-menu").toggleClass("is-show");
+    // $(".p-drawer-menu").toggleClass("is-show");
+    $(".js-drawer-open").toggleClass("open");
+    $(".drawer-menu").toggleClass("open");
+    $("html").toggleClass("is-fixed");
+
+  });
+});
