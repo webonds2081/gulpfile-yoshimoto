3f24da3d562f2aa3b7e52ed8c60b598789ad98ec
webonds2081 <webonds2081@gmail.com>
2021-07-16 09:31:04 +0900

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
+"use strict";
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
   var topBtn = $('.pagetop');
-  topBtn.hide();
+  topBtn.hide(); // ボタンの表示設定
 
-  // ボタンの表示設定
   $(window).scroll(function () {
     if ($(this).scrollTop() > 70) {
       // 指定px以上のスクロールでボタンを表示
@@ -13,38 +13,32 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
       // 画面が指定pxより上ならボタンを非表示
       topBtn.fadeOut();
     }
-  });
+  }); // ボタンをクリックしたらスクロールして上に戻る
 
-  // ボタンをクリックしたらスクロールして上に戻る
   topBtn.click(function () {
     $('body,html').animate({
       scrollTop: 0
     }, 300, 'swing');
     return false;
-  });
+  }); //ドロワーメニュー
 
-  //ドロワーメニュー
   $("#MenuButton").click(function () {
     // $(".l-drawer-menu").toggleClass("is-show");
     // $(".p-drawer-menu").toggleClass("is-show");
     $(".js-drawer-open").toggleClass("open");
     $(".drawer-menu").toggleClass("open");
     $("html").toggleClass("is-fixed");
-
-  });
-
-
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
+  }); // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
 
   $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
+    var time = 400;
+    var header = $('header').innerHeight();
+    var target = $(this.hash);
     if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
+    var targetY = target.offset().top - header;
+    $('html,body').animate({
+      scrollTop: targetY
+    }, time, 'swing');
     return false;
   });
-
 });
